75959c0f2ea9357e7de35cadddbf61838fcfce44
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-08-02 20:04:06 -0700

    Stub methods.

--- www/www.js
+++ www/www.js
@@ -1,3 +1,28 @@
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  update:function(root) {
+    root = root || "";
+    // Load in Github
+    var GithubApi = Meteor.require('github');
+    var github = new GithubApi({
+      version: "3.0.0"
+    });
+    // Load in xml2js
+    var parseXml = Meteor.require('xml2js').parseString;
+    // Pull latest from git
+    
+    // Download from US code website into raw
+
+    // Parse US Code
+
+    // Shuffle into appropriate directories
+
+    // Commit
+
+    // Upload
+
+  }
+});
